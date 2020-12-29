a549f5bf7576b6cd13b4bb4094a4267b0d91c183
Luis Gustavo <luisgbyte>
2020-12-29 16:54:39 -0300

    main structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import './App.css';
+import { Router } from 'react-router-dom';
+
+import Routes from './routes';
+import history from './services/history';
+
+import GlobalStyle from './styles/global';
 
 function App() {
     return (
-        <div>
-            <h2>hello world!</h2>
-        </div>
+        <Router history={history}>
+            <Routes />
+            <GlobalStyle />
+        </Router>
     );
 }
 
