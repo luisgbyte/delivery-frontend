c51ca50e4fa91a1898bf60d9adf1293461a9356e
Luis Gustavo <luisgbyte>
2020-12-30 17:50:19 -0300

    signIn layout

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,26 +4,41 @@ module.exports = {
         es2021: true,
     },
     extends: [
-        "plugin:react/recommended",
-        "airbnb",
-        "prettier",
-        "prettier/react",
+        'plugin:react/recommended',
+        'airbnb',
+        'prettier',
+        'prettier/react',
     ],
-    parser: "babel-eslint",
+    parser: 'babel-eslint',
     parserOptions: {
         ecmaFeatures: {
             jsx: true,
         },
         ecmaVersion: 12,
-        sourceType: "module",
+        sourceType: 'module',
     },
-    plugins: ["react", "prettier"],
+    plugins: ['react', 'prettier'],
     rules: {
-        "prettier/prettier": "error",
-        "react/jsx-filename-extension": [
-            "warn",
-            { extensions: [".jsx", ".js"] },
+        'prettier/prettier': 'error',
+        'react/jsx-filename-extension': [
+            'warn',
+            {
+                extensions: ['.jsx', '.js'],
+            },
         ],
-        "import/prefer-default-export": "off",
+        'import/prefer-default-export': 'off',
+        'react/state-in-constructor': 'off',
+        'react/static-property-placement': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        'react/prop-types': 'off',
+        'no-param-reassign': 'off',
+        'no-console': 'off',
+    },
+    settings: {
+        'import/resolver': {
+            'babel-plugin-root-import': {
+                rootPathSuffix: 'src',
+            },
+        },
     },
 };
