618f6eb90333f053359b705e9c61a5590bed1472
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-01-01 20:50:58 -0300

    notification logic

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
-
+import React, { useState, useEffect, useMemo } from 'react';
 import { MdNotifications } from 'react-icons/md';
+import { parseISO, formatDistance } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import api from '~/services/api';
 
 import {
     Container,
@@ -11,39 +14,81 @@ import {
 } from './styles';
 
 function Notifications() {
+    const [visible, setVisible] = useState(false);
+    const [notifications, setNotifications] = useState([]);
+
+    const hasUnread = useMemo(
+        () =>
+            !!notifications.find((notification) => notification.read === false),
+
+        [notifications]
+    );
+
+    useEffect(() => {
+        async function loadNotifications() {
+            const response = await api.get('notifications');
+
+            const data = response.data.map((notification) => ({
+                ...notification,
+                timeDistance: formatDistance(
+                    parseISO(notification.createdAt),
+                    new Date(),
+                    { addSuffix: true, locale: pt }
+                ),
+            }));
+
+            setNotifications(data);
+        }
+
+        loadNotifications();
+    }, []);
+
+    function handleToggleVisible() {
+        setVisible(!visible);
+    }
+
+    async function handleMarkAsRead(id) {
+        await api.put(`notifications/${id}`);
+
+        setNotifications(
+            notifications.map((notification) =>
+                // eslint-disable-next-line no-underscore-dangle
+                notification._id === id
+                    ? { ...notification, read: true }
+                    : notification
+            )
+        );
+    }
+
     return (
         <Container>
-            <Badge hasUnread>
+            <Badge onClick={handleToggleVisible} hasUnread={hasUnread}>
                 <MdNotifications color="#c72828" size={20} />
             </Badge>
 
-            <NotificationList>
+            <NotificationList visible={visible}>
                 <Scroll>
-                    <Notification unread>
-                        <p>Novo pedido de cliente1 01/01/2021</p>
-                        <time>há 5 minutos</time>
-                        <button type="button">Marcar como lida</button>
-                    </Notification>
-                    <Notification>
-                        <p>Novo pedido de cliente1 01/01/2021</p>
-                        <time>há 5 minutos</time>
-                        <button type="button">Marcar como lida</button>
-                    </Notification>
-                    <Notification>
-                        <p>Novo pedido de cliente1 01/01/2021</p>
-                        <time>há 5 minutos</time>
-                        <button type="button">Marcar como lida</button>
-                    </Notification>
-                    <Notification>
-                        <p>Novo pedido de cliente1 01/01/2021</p>
-                        <time>há 5 minutos</time>
-                        <button type="button">Marcar como lida</button>
-                    </Notification>
-                    <Notification>
-                        <p>Novo pedido de cliente1 01/01/2021</p>
-                        <time>há 5 minutos</time>
-                        <button type="button">Marcar como lida</button>
-                    </Notification>
+                    {notifications.map((notification) => (
+                        <Notification
+                            // eslint-disable-next-line no-underscore-dangle
+                            key={notification._id}
+                            unread={!notification.read}
+                        >
+                            <p>{notification.content}</p>
+                            <time>{notification.timeDistance}</time>
+                            {!notification.read && (
+                                <button
+                                    type="button"
+                                    onClick={() =>
+                                        // eslint-disable-next-line no-underscore-dangle
+                                        handleMarkAsRead(notification._id)
+                                    }
+                                >
+                                    Marcar como lida
+                                </button>
+                            )}
+                        </Notification>
+                    ))}
                 </Scroll>
             </NotificationList>
         </Container>
