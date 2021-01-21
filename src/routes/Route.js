62243256b0a3323a037481d07a75a695ff43d706
luisgbyte <44074498+luisgbyte@users.noreply.github.com>
2021-01-21 20:29:18 -0300

    getting state 'signed'

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -2,17 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
+import { useSelector } from 'react-redux';
 import AuthLayout from '~/pages/_layouts/auth';
 import DefaultLayout from '~/pages/_layouts/default';
-
-import { store } from '~/store';
 
 export default function RouteWrapper({
     component: Component,
     isPrivate,
     ...rest
 }) {
-    const { signed } = store.getState().auth;
+    const { signed } = useSelector((state) => state.auth);
 
     if (!signed && isPrivate) {
         return <Redirect to="/" />;
